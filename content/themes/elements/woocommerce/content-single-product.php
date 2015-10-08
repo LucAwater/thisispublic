8d9e743bec40c12cf1deffeeffb5f56213169c68
Luc Awater <lucawater@gmail.com>
2015-10-08 12:09:41 +0200

    Display product description

--- content/themes/elements/woocommerce/content-single-product.php
+++ content/themes/elements/woocommerce/content-single-product.php
@@ -43,20 +43,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="summary entry-summary">
 
-		<?php
-			/**
-			 * woocommerce_single_product_summary hook
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
+    <?php
+    $title = get_the_title();
+    $description = get_field('description');
+
+    echo '<h1 itemprop="name">' . $title . '</h1>';
+    echo '<div class="product-description">' . $description . '</div>';
+
+    // @hooked woocommerce_template_single_add_to_cart - 30
+    do_action( 'woocommerce_single_product_summary' );
+    ?>
 
 	</div><!-- .summary -->
 
