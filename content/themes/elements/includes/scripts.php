92c93445c6a1ed086eaf91e81fdb3a00df6bb912
Luc Awater <lucawater@gmail.com>
2015-10-12 15:57:17 +0200

    Tweak typography
    Added a script that detects wether or not an a tag has text, dev or img elements. It then gives the a tag a class so the underline css property can be removed.

--- content/themes/elements/includes/scripts.php
+++ content/themes/elements/includes/scripts.php
@@ -19,6 +19,7 @@ function enqueue_theme_scripts() {
     wp_enqueue_script( 'pageload', get_template_directory_uri() . '/js/pageload.js', 'jquery', false, true );
     wp_enqueue_script( 'resize', get_template_directory_uri() . '/js/resize.js', 'jquery', false, true );
     wp_enqueue_script( 'hero', get_template_directory_uri() . '/js/hero.js', 'jquery', false, true );
+    wp_enqueue_script( 'link-heading', get_template_directory_uri() . '/js/link-heading.js', 'jquery', false, true );
     wp_enqueue_script( 'init-isotope', get_template_directory_uri() . '/js/init-isotope.js', 'jquery', false, true );
     wp_enqueue_script( 'init-skrollr', get_template_directory_uri() . '/js/init-skrollr.js', 'jquery', false, true );
  	else:
