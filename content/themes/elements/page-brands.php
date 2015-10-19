2d1d2b0b16a6bd13f7693f56e8e4700e013793cc
Luc Awater <lucawater@gmail.com>
2015-10-19 13:18:42 +0200

    Add brand overview page

---
+++ content/themes/elements/page-brands.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/archive-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+		<?php
+			/**
+			 * woocommerce_archive_description hook
+			 *
+			 * @hooked woocommerce_taxonomy_archive_description - 10
+			 * @hooked woocommerce_product_archive_description - 10
+			 */
+			do_action( 'woocommerce_archive_description' );
+		?>
+
+    <?php
+    // Check if user is logged in
+    if( ! is_user_logged_in() ):
+      echo '<p class="woocommerce-info">You must be logged in to view products.</p>';
+    // If user is logged in, continue looping products
+    else:
+      echo do_shortcode('[product_brand_list]');
+    endif;
+    ?>
+
+	<?php
+		/**
+		 * woocommerce_after_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+	<?php
+		/**
+		 * woocommerce_sidebar hook
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php get_footer( 'shop' ); ?>
--- content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
+++ content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
@@ -4,9 +4,8 @@
  *
  * @usedby [product_brand_list]
  */
-echo '<ul class="brands">';
-
-  if (is_tax( 'product_brand' )) {
+if (is_tax( 'product_brand' )) {
+  echo '<ul class="brands">';
     $terms = get_the_terms($post->id, 'product_brand');
     $current_brand = $terms[0]->slug;
 
@@ -21,15 +20,33 @@ echo '<ul class="brands">';
         }
       }
     endif;
-  } else {
-    echo '<li class="current"><a href="' . get_permalink( woocommerce_get_page_id( 'shop' ) ) . '">All</a></li>';
-
+  echo '</ul>';
+} else {
+  echo '<ul class="brands s-grid-1 m-grid-2 isotope isotope-masonry">';
     foreach ( $index as $i ) if ( isset( $product_brands[ $i ] ) ) :
       foreach ( $product_brands[ $i ] as $brand ) {
-        echo '<li><a href="' . get_term_link( $brand->slug, 'product_brand' ) . '">' . $brand->name . '</a></li>';
+        // echo $brand->term_id;
+        // echo $brand->name;
+        // echo $brand->slug;
+        // echo $brand->term_group;
+        // echo $brand->term_taxonomy_id;
+        // echo $brand->taxonomy;
+        // echo $brand->description;
+        // echo $brand->parent;
+        // echo $brand->count;
+
+        echo
+        '<li class="brand">
+          <a href="' . get_term_link( $brand->slug, 'product_brand' ) . '">
+            <div>
+              <h3>' . $brand->name . '</h3>
+              <p>' . $brand->description . '</p>
+              <img src="' . get_brand_thumbnail_url( $brand->term_id ) . '">
+            </div>
+          </a>
+        </li>';
       }
     endif;
-  }
-
-echo '</ul>';
+  echo '</ul>';
+}
 ?>
