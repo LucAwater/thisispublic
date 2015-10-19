6fb5d1b52066f588bc50a634a179e397bcbb9faf
Luc Awater <lucawater@gmail.com>
2015-10-19 17:25:42 +0200

    Fix shop page

--- content/themes/elements/woocommerce/archive-product.php
+++ content/themes/elements/woocommerce/archive-product.php
@@ -41,91 +41,96 @@ get_header( 'shop' ); ?>
       echo '<p class="woocommerce-info">You must be logged in to view products.</p>';
     // If user is logged in, continue looping products
     else:
-      echo '<div class="filter">';
-
-        // Brand filter
-        // Get current brand
-        // if (is_tax( 'product_brand' )) {
-        //   $terms = get_the_terms($post->id, 'product_brand');
-        //   echo '<p>' . $terms[0]->name . '</p>';
-        // } else {
-        //   echo '<p>All</p>';
-        // }
-
-        // List rest of brands
+      if( is_shop() ){
         echo do_shortcode('[product_brand_list]');
+      } else {
+        echo '<div class="filter">';
 
-        // Gender filter
-        $genders = $terms = get_terms( 'gender', 'orderby=count&hide_empty=0' );
-        echo '<ul id="select-gender" name="gender">';
-        echo '<li><a class="tax-filter" title="all">All</a></li>';
-        foreach ( $genders as $gender ) {
-          echo '<li><a class="tax-filter" title="' . $gender->slug . '">' . $gender->name . '</a></li>';
-        }
-        echo '</ul>';
+          // Brand filter
+          // Get current brand
+          // if (is_tax( 'product_brand' )) {
+          //   $terms = get_the_terms($post->id, 'product_brand');
+          //   echo '<p>' . $terms[0]->name . '</p>';
+          // } else {
+          //   echo '<p>All</p>';
+          // }
 
-        // Search
-        echo
-        '<a class="search" href="javascript:;">
-          <form role="search" method="get" class="search-form" action="' . home_url( '/' ) . '">
-            <input type="search" class="search" placeholder="Search" value="" name="s" title="" />
-          </form>
-        </a>';
+          // List rest of brands
+          echo do_shortcode('[product_brand_list]');
 
-      echo '</div>';
+          // Gender filter
+          $genders = $terms = get_terms( 'gender', 'orderby=count&hide_empty=0' );
+          echo '<ul id="select-gender" name="gender">';
+          echo '<li><a class="tax-filter" title="all">All</a></li>';
+          foreach ( $genders as $gender ) {
+            echo '<li><a class="tax-filter" title="' . $gender->slug . '">' . $gender->name . '</a></li>';
+          }
+          echo '</ul>';
 
-      if ( have_posts() ) : ?>
+          // Search
+          echo
+          '<a class="search" href="javascript:;">
+            <form role="search" method="get" class="search-form" action="' . home_url( '/' ) . '">
+              <input type="search" class="search" placeholder="Search" value="" name="s" title="" />
+            </form>
+          </a>';
 
-        <!-- <?php
-          /**
-           * woocommerce_before_shop_loop hook
-           *
-           * @hooked woocommerce_result_count - 20
-           * @hooked woocommerce_catalog_ordering - 30
-           */
-          do_action( 'woocommerce_before_shop_loop' );
-        ?> -->
+        echo '</div>';
 
-        <?php woocommerce_product_loop_start(); ?>
+        if ( have_posts() ) : ?>
 
-          <?php woocommerce_product_subcategories(); ?>
+          <!-- <?php
+            /**
+             * woocommerce_before_shop_loop hook
+             *
+             * @hooked woocommerce_result_count - 20
+             * @hooked woocommerce_catalog_ordering - 30
+             */
+            do_action( 'woocommerce_before_shop_loop' );
+          ?> -->
 
-          <?php while ( have_posts() ) : the_post(); ?>
+          <?php woocommerce_product_loop_start(); ?>
 
-            <?php
-            if ( is_user_logged_in() ){
-              global $current_user;
-              $current_user_role = $current_user->roles[0];
+            <?php woocommerce_product_subcategories(); ?>
 
-              // Custom user level
-              $user_level = get_field( 'user_level' );
+            <?php while ( have_posts() ) : the_post(); ?>
 
-              if( $current_user_role === 'administrator' || in_array($current_user_role, $user_level) ){
-                wc_get_template_part( 'content', 'product' );
+              <?php
+              if ( is_user_logged_in() ){
+                global $current_user;
+                $current_user_role = $current_user->roles[0];
+
+                // Custom user level
+                $user_level = get_field( 'user_level' );
+
+                if( $current_user_role === 'administrator' || in_array($current_user_role, $user_level) ){
+                  wc_get_template_part( 'content', 'product' );
+                }
               }
-            }
-            ?>
+              ?>
 
-          <?php endwhile; // end of the loop. ?>
+            <?php endwhile; // end of the loop. ?>
 
-        <?php woocommerce_product_loop_end(); ?>
+          <?php woocommerce_product_loop_end(); ?>
+
+          <?php
+            /**
+             * woocommerce_after_shop_loop hook
+             *
+             * @hooked woocommerce_pagination - 10
+             */
+            do_action( 'woocommerce_after_shop_loop' );
+          ?>
 
         <?php
-          /**
-           * woocommerce_after_shop_loop hook
-           *
-           * @hooked woocommerce_pagination - 10
-           */
-          do_action( 'woocommerce_after_shop_loop' );
-        ?>
+        elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
 
-      <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+          <?php wc_get_template( 'loop/no-products-found.php' ); ?>
 
-        <?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-      <?php endif; ?>
-    <?php endif;
-    ?>
+        <?php
+        endif;
+      }// end of product loop
+    endif; ?><!-- end of user login check -->
 
 	<?php
 		/**
