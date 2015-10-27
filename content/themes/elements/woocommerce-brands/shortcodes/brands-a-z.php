6e1355fcfbd5e2fac9a24e85bd0725d3b4691a8b
Luc Awater <lucawater@gmail.com>
2015-10-27 10:46:34 +0100

    Adapt block grids to new query ranges

--- content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
+++ content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
@@ -26,7 +26,7 @@ if( is_tax('product_brand') || is_shop() || is_search() ){
     endif;
   echo '</ul>';
 } else {
-  echo '<ul class="brands s-grid-1 m-grid-2 l-grid-3 isotope isotope-masonry">';
+  echo '<ul class="brands s-grid-1 l-grid-2 xl-grid-3 isotope isotope-masonry">';
     foreach ( $index as $i ) if ( isset( $product_brands[ $i ] ) ) :
       foreach ( $product_brands[ $i ] as $brand ) {
         // echo $brand->term_id;
