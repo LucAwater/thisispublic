ce10adbd20a07e30cd8fd2b156c5aa6ee9f6b8d1
Luc Awater <lucawater@gmail.com>
2015-12-15 14:22:08 +0100

    Rebuild brand thumbs

--- content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
+++ content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
@@ -41,29 +41,37 @@ if( is_tax('product_brand') || is_shop() || is_search() ){
   echo '<ul class="brands s-grid-1 l-grid-2 xl-grid-3 isotope isotope-masonry">';
     foreach ( $index as $i ) if ( isset( $product_brands[ $i ] ) ) :
       foreach ( $product_brands[ $i ] as $brand ) {
-        // echo $brand->term_id;
-        // echo $brand->name;
-        // echo $brand->slug;
-        // echo $brand->term_group;
-        // echo $brand->term_taxonomy_id;
-        // echo $brand->taxonomy;
-        // echo $brand->description;
-        // echo $brand->parent;
-        // echo $brand->count;
 
-        echo
-        '<li class="brand">
+        // Build thumb
+        ?>
+        <li class="brand">
           <div>
-            <a href="' . get_term_link( $brand->slug, 'product_brand' ) . '" data-hover="view this brand">
-              <div class="thumb-content">
-                <h3>' . $brand->name . '</h3>
-                <p>' . $brand->description . '</p>
-              </div>
-              <img src="' . get_brand_thumbnail_url( $brand->term_id ) . '">
-              <div class="overlay-thumb"><p>view brand</p></div>
+            <div class="thumb-content">
+              <a href="<?php echo get_term_link( $brand->slug, 'product_brand' ); ?>"><h3><?php echo $brand->name; ?></h3></a>
+            </div>
+
+            <a href="<?php echo get_term_link( $brand->slug, 'product_brand' ); ?>">
+              <img src="<?php echo get_brand_thumbnail_url( $brand->term_id ); ?>">
             </a>
+
+            <div class="thumb-content">
+              <p><?php echo $brand->description; ?></p>
+              <?php
+              // Brand files
+              $brand_files = get_field( 'brand_files', $brand );
+              foreach( $brand_files as $file ){
+                $url = $file['brand_files_file']['url'];
+                $title = $file['brand_files_title'];
+
+                if(! empty($url) ){
+                  echo '<a class="link-arrow link-arrow-right" href="' . $url . '"><img src="' . get_template_directory_uri() . '/img/arrow.svg">' . $title . '</a>';
+                }
+              }
+              ?>
+            </div>
           </div>
-        </li>';
+        </li>
+        <?php
       }
     endif;
   echo '</ul>';
