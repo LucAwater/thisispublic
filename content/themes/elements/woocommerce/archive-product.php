9aa0646873e6792476e2d7e5b2d078aa770fc93f
Luc Awater <lucawater@gmail.com>
2015-12-16 09:53:31 +0100

    Build query on search page

--- content/themes/elements/woocommerce/archive-product.php
+++ content/themes/elements/woocommerce/archive-product.php
@@ -85,9 +85,9 @@ get_header( 'shop' ); ?>
 
         // Search
         echo
-        '<form role="search" method="get" class="search-form" action="' . home_url( '/' ) . '">
-          <input type="search" class="search" value="" name="s" title="" />
-          <button type="submit">search</button>
+        '<form id="search-form" role="search" method="get" class="search-form" action="' . home_url( '/' ) . '">
+          <input id="search-field" type="search" class="search" value="" name="s" title="" />
+          <button id="search-button" type="submit">search</button>
           <p class="search-trigger">search</p>
         </form>';
 
