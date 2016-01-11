413a01a475d490fe36fa3592d1a00eec414276bf
Luc Awater <lucawater@gmail.com>
2016-01-11 16:14:38 +0100

    Tweak brand thumb styling

--- content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
+++ content/themes/elements/woocommerce-brands/shortcodes/brands-a-z.php
@@ -57,22 +57,29 @@ if( is_tax('product_brand') || is_shop() || is_search() ){
 
             <div class="thumb-content">
               <p><?php echo $brand->description; ?></p>
-              <a class="link-arrow link-arrow-right" href="<?php echo get_term_link( $brand->slug, 'product_brand' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow.svg">View products from this brand</a>
+              <a class="button button-sec" href="<?php echo get_term_link( $brand->slug, 'product_brand' ); ?>">View products</a>
 
               <?php
               // Brand files
               $brand_files = get_field( 'brand_files', $brand );
               if( $brand_files ):
+
+                echo '<div class="brand-files">';
+
                 foreach( $brand_files as $file ){
                   $url = $file['brand_files_file']['url'];
                   $title = $file['brand_files_title'];
 
                   echo '<a class="link-arrow link-arrow-right" href="' . $url . '"><img src="' . get_template_directory_uri() . '/img/arrow.svg">' . $title . '</a>';
                 }
+
+                echo '</div>';
               endif;
 
               $brand_mailLinks = get_field( 'brand_mailLinks', $brand );
               if( $brand_mailLinks ):
+                echo '<div class="brand-links">';
+
                 foreach( $brand_mailLinks as $link ){
                   $subject = $link['brand_mailLinks_subject'];
                   $content = $link['brand_mailLinks_content'];
@@ -80,6 +87,8 @@ if( is_tax('product_brand') || is_shop() || is_search() ){
 
                   echo '<a class="link-arrow link-arrow-right" href="mailto:info@thisispublic.com?subject=' . $subject . '&body=' . $content . '"><img src="' . get_template_directory_uri() . '/img/arrow.svg">' . $actionlink . '</a>';
                 }
+
+                echo '</div>';
               endif;
               ?>
             </div>
